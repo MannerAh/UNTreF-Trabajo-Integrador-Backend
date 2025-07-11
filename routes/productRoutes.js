23213fe8a83162bcd2a7d2366a6d93b56375a5fb
MannerAh <leoncitobaro@hotmail.es>
2025-07-11 13:19:15 -0300

    feat: routers for index

---
+++ routes/productRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { getAllProducts, getProductByCode, postProduct, patchProduct, deleteByCode } = require('../controllers/productController');
+const router = express.Router();
+
+// GET /productos
+router.get('/', getAllProducts);
+
+// POST /productos
+router.post('/', postProduct);
+
+// GET /productos/buscar?q={termino_de_busqueda}
+router.get('/buscar', getProductByQuery);
+
+// GET /productos/categoria/:nombre
+router.get('/categoria/:nombre', getProductByCategory);
+
+// GET /productos/precio/:min-:max
+router.get('/precio/:min-:max', getProductByPrice);
+
+// POST /productos/masivo
+router.post('/masivo', scanProducts);
+
+// GET /productos/:codigo
+router.get('/:codigo', getProductByCode);
+
+// PUT /productos/:codigo
+router.put('/:codigo', patchProduct);
+
+// DELETE /productos/:codigo
+router.delete('/:codigo', deleteByCode);
+
+module.exports = router;
