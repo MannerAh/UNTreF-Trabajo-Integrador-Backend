7f52f64526066fa27645fc31ea4a4c75b59eb904
MannerAh <leoncitobaro@hotmail.es>
2025-07-13 20:48:52 -0300

    all apps working

--- app.js
+++ app.js
@@ -7,22 +7,35 @@ app.use(express.json());
 // Routes
 
 // GET /productos
-app.get('/productos', require('./routes/productRoutes'));
+app.use('/productos', require('./routes/productRoutes'));
 
 // POST /productos
-app.post('/productos', require('./routes/productRoutes'));
-
-
+app.use('/productos', require('./routes/productRoutes'));
 
 // GET /productos/:codigo
-app.get('/:codigo', getProductByCode);
+app.use('/:codigo', require('./routes/productRoutes'));
 
 // PUT /productos/:codigo
-app.put('/:codigo', patchProduct);
+app.use('/:codigo', require('./routes/productRoutes'));
 
 // DELETE /productos/:codigo
-app.delete('/:codigo', deleteByCode);
+app.use('/:codigo', require('./routes/productRoutes'));
+
+
+// Endpoints adicionales
+
+// GET /productos/buscar?q={termino_de_busqueda}
+app.use('/productos/buscar', require('./routes/productRoutes'));
+
+// GET /productos/categoria/:nombre
+app.use('/productos/categoria/:nombre', require('./routes/productRoutes'));
+
+// GET /productos/precio/:min-:max
+app.use('/productos/precio/:min-:max', require('./routes/productRoutes'));
+
+// POST /productos/masivo
+app.use('/productos/masivo', require('./routes/productRoutes'));
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
+    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
